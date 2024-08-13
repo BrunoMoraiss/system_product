fe5a4a59ce950a3681314ad634279b4ee5ee7a4f
Bruno Morais <brunomoraisads@gmail.com>
2024-08-13 11:28:34 -0300

    :recycle: refactor: Implementação do mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
+import { ContactsModule } from './contacts/contacts.module';
 import { DatabaseModule } from './database/database.module';
 import { LoggerModule } from './logger/logger.module';
+import { MessagesModule } from './messages/messages.module';
 import { QrcodeModule } from './qrcode/qrcode.module';
 import { ServicesModule } from './services/services.module';
 
@@ -8,9 +10,26 @@ import { AppService } from './app.service';
 import { LoggerService } from './logger/logger.service';
 
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [LoggerModule, DatabaseModule, ServicesModule, QrcodeModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.DATABASE_URI, {
+      dbName: process.env.DATABASE_NAME,
+      auth: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+      },
+    }),
+    LoggerModule,
+    DatabaseModule,
+    ServicesModule,
+    QrcodeModule,
+    MessagesModule,
+    ContactsModule,
+  ],
   controllers: [AppController],
   providers: [AppService, LoggerService],
 })
